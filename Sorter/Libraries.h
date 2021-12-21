c0893c4dd13ed7f22b05788c49fbb1876470f47d
Sveta Sveta <akai.iti@gmail.com>
2021-12-21 15:48:53 +0200

    21.12.2021

---
+++ Sorter/Libraries.h
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <ctime>
+using namespace std;
+
+
--- Sorter/Sorter.cpp
+++ Sorter/Sorter.cpp
@@ -1,60 +1,15 @@
 ﻿// Sorter.cpp : Создать класс Sorter, который будет содержать в себе статические методы, реализующие разные алгоритмы сортировки одномерных массивов 
 //(Bubble, Insert, Select, Quick). Массивы могут быть любого типа, любых размеров(по количеству элементов).
 
-#include <iostream>
-#include <ctime>
-using namespace std;
+#include "Libraries.h"
+
+template <class T>
 
 class Sorter
-{
-    int size; 
-    int* arr;    
+{    
+public:    
 
-public:
-        
-    Sorter(int* _arr, int _size)
-    {        
-        Show(_arr, _size);
-    }
-    void Show(int* _arr, int _size)
-    {
-        if (_size <= 0)
-        {
-            arr = nullptr;
-            size = 0;
-            return;
-        }
-        size = _size;
-        arr = new int[_size];
-        for (int i = 0; i < size; i++)
-        {
-            arr[i] = _arr[i];
-        }
-    }
-    void SetArr(int* _arr)
-    {        
-        for (int i = 0; i < size; i++)
-        {
-            arr[i] = _arr[i];
-        } 
-    }    
-    void SetSize(int size)
-    {
-        this->size = size;
-    }
-    ~Sorter()
-    {
-        if (arr != nullptr) delete[] arr;       
-    }
-    const int* const GetArr() const
-    {
-        return arr;
-    }
-    const int const GetSize() const
-    {
-        return size;
-    }
-    void Bubble(int size)
+    static void Bubble(T arr[], int size)
     {
         int tmp = 0;
         for (int i = 0; i < size; i++) {
@@ -63,11 +18,12 @@ public:
                     tmp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = tmp;
-                }
-            }
-        }
+                }                
+            }   
+            cout << arr[i] << " ";
+        }              
     }
-    void Insert(int size)
+    static void Insert(T arr[], int size)
     {
         int value = 0;
         int i = 0;
@@ -79,12 +35,13 @@ public:
                 i = i - 1;
                 arr[i + 1] = value;
             }
-        }
+            cout << arr[i] << " ";
+        }       
     }
-    void Select(int size)
+    static void Select(T arr[], int size)
     {
         int j = 0;
-        int tmp = 0;
+        T tmp = 0;
         for (int i = 0; i < size; i++) {
             j = i;
             for (int k = i; k < size; k++) {
@@ -95,11 +52,12 @@ public:
             tmp = arr[i];
             arr[i] = arr[j];
             arr[j] = tmp;
-        }
+            cout << arr[i] << " ";
+        }        
     }
-    void Quick(int size)
-    {       
-        int m = 0;
+    static void Quick(T arr[], int size)
+    {
+        T m = 0;
         int index = size / 2;
         int i, j = 0, k = 0;
         if (size > 1) {
@@ -117,43 +75,33 @@ public:
                         k++;
                     }
                 }
-            }          
+                cout << arr[i] << " ";
+            }
         }        
-    }
-    void Print()
-    {        
-        for (int i = 0; i < size; i++)
-            cout << arr[i] << " ";
-        cout << "\n";
-    }
+    }  
 };
-
-
 int main()
 {
     srand(time(0));
     const int size = 10;
     int arr[size];
     cout << "Array \n";
-    for (int i = 0; i < size; i++) {
-
+    for (int i = 0; i < size; i++)
+    {
         arr[i] = rand() % 100;
         cout << arr[i] << " ";
     }
+    cout << "\n";    
+    cout << "Bubble\n";
+    Sorter<int>::Bubble(arr, 10);
+    cout << "\n"; 
+    cout << "Insert\n"; 
+    Sorter<int>::Insert(arr, 10);
     cout << "\n";
-
-    Sorter* s = new Sorter(arr,10);
-    cout << "Bubble \n";
-    s->Bubble(size);
-    s->Print();
-    cout << "Insert \n";
-    s->Insert(size);
-    s->Print();
-    cout << "Select \n";
-    s->Select(size);
-    s->Print();
-    cout << "Quick \n";
-    s->Quick(size);
-    s->Print();
+    cout << "Select\n";
+    Sorter<int>::Select(arr, 10);
+    cout << "\n";
+    cout << "Quick\n";
+    Sorter<int>::Quick(arr, 10);
     cout << "\n\n";
 }
